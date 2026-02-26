06c03d2822b1cb53bcbecfc1449851a4cf7b6593
Zackp5600 <zackp5600@gmail.com>
2026-02-26 12:06:08 -0500

    added stuff from yt video

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+const UserDetails = () => {
+  return (
+    <div>UserDetails</div>
+  )
+}
+
+export default UserDetails
---
+++ components/Hello.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+const Hello = () => {
+    console.log("I am a client component")
+  return (
+    <div>hello</div>
+  )
+}
+
+export default Hello
